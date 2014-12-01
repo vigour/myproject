359ce26145cbc11590185dd486b7a3d854e7ebe7
vigour <vigogiv@hotmail.com>
2014-12-01 23:57:15 +0800

    修改UUID类的结构，待续。。

--- src/main/java/com/keitsen/demo/basic/entity/AbstractEntity.java
+++ src/main/java/com/keitsen/demo/basic/entity/AbstractEntity.java
@@ -1,13 +1,121 @@
 package com.keitsen.demo.basic.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+import javax.xml.bind.annotation.XmlTransient;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
-public class AbstractEntity implements Serializable{
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable{
 
-	private static final long serialVersionUID = 5639006408483990605L;
+	@Transient
+	protected static final long serialVersionUID = 5639006408483990605L;
+	
+	/**
+	 * 是否显示
+	 */
+	protected boolean visible = true;
+	
+	/**
+	 * 状态
+	 */
+	protected int status;
+	
+	/**
+	 * 创建时间
+	 */
+	protected Date creationDate;
+	
+	
+	/**
+	 * 最后修改时间
+	 */
+	protected Date modificationDate;
+	
+	
+	/**
+	 * 备注
+	 */
+	protected String remark;
+	
+	/**
+     * 版本号 乐观锁
+     */
+    protected int version;
+    
+    
+    
+	@Column(nullable = false)
+	public boolean isVisible() {
+		return visible;
+	}
 
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	
+	@Column(name="status")
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "creattion_date", nullable = false, updatable = false)
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "modification_date")
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(Date modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+	
+
+	@Column(name = "remark",length=200)
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	@Version
+    @Column(name = "version")
+	@XmlTransient
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+
+	
+	
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
--- src/main/java/com/keitsen/demo/basic/entity/UUIDEntity.java
+++ src/main/java/com/keitsen/demo/basic/entity/UUIDEntity.java
@@ -1,7 +1,5 @@
 package com.keitsen.demo.basic.entity;
 
-import java.util.Date;
-
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.FetchType;
@@ -10,9 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Version;
 
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
@@ -25,47 +20,12 @@ import com.keitsen.demo.module.user.entity.User;
 @MappedSuperclass
 public class UUIDEntity extends AbstractEntity {
 
-	private String id;
+	protected String id;
 	
-	/**
-	 * 是否显示
-	 */
-	private boolean visible = true;
+	protected User creator;
 	
-	/**
-	 * 状态
-	 */
-	private int status;
+	protected User modifier;
 	
-	/**
-	 * 创建时间
-	 */
-	private Date creationDate;
-	
-	/**
-	 * 创建人
-	 */
-	private User creator;	
-	
-	/**
-	 * 最后修改时间
-	 */
-	private Date modificationDate;
-	
-	/**
-	 * 修改人
-	 */
-	private User modifier;	
-	
-	/**
-	 * 备注
-	 */
-	private String remark;
-	
-	/**
-     * 版本号 乐观锁
-     */
-    private int version;
 
 	@Id
 	@Column(name = "ID", unique = true, nullable = false, insertable = true, updatable = true, length = 32)
@@ -79,33 +39,6 @@ public class UUIDEntity extends AbstractEntity {
 		this.id = id;
 	}
 	
-	@Column(nullable = false)
-	public boolean isVisible() {
-		return visible;
-	}
-
-	public void setVisible(boolean visible) {
-		this.visible = visible;
-	}
-	
-	@Column(name="status")
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "creattion_date", nullable = false, updatable = false)
-	public Date getCreationDate() {
-		return creationDate;
-	}
-
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
 	
 	@ManyToOne(targetEntity = User.class)
     @JoinColumn(name = "creator")
@@ -119,18 +52,9 @@ public class UUIDEntity extends AbstractEntity {
 		this.creator = creator;
 	}
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "modification_date")
-	public Date getModificationDate() {
-		return modificationDate;
-	}
-
-	public void setModificationDate(Date modificationDate) {
-		this.modificationDate = modificationDate;
-	}
 	
 	@ManyToOne(targetEntity = User.class)
-    @JoinColumn(name = "modifier")
+	@JoinColumn(name = "modifier")
 	@Fetch(FetchMode.SELECT)
     @Basic(fetch = FetchType.LAZY)
 	public User getModifier() {
@@ -141,25 +65,33 @@ public class UUIDEntity extends AbstractEntity {
 		this.modifier = modifier;
 	}
 
-	@Column(name = "remark",length=200)
-	public String getRemark() {
-		return remark;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
-	public void setRemark(String remark) {
-		this.remark = remark;
-	}
-
-	@Version
-    @Column(name = "version")
-	public int getVersion() {
-		return version;
-	}
-
-	public void setVersion(int version) {
-		this.version = version;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UUIDEntity other = (UUIDEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
+	
+
 	
 	
 	
